0510fdf207c9a161cd4ab28fb3298618eef8166f
daiqiang <daiqiang@youzan.com>
2017-09-09 18:06:54 +0800

    Merge branch 'dev' of github.com:wumoyu850921/httpfetch into dev
    # Conflicts:
    #	src/main/java/com/github/nezha/httpfetch/HttpApiService.java
    #	src/main/java/com/github/nezha/httpfetch/resolver/BeanMethodParameterResolver.java

--- src/main/java/com/github/nezha/httpfetch/HttpApiService.java
+++ src/main/java/com/github/nezha/httpfetch/HttpApiService.java
@@ -12,7 +12,10 @@ import org.slf4j.LoggerFactory;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.*;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
